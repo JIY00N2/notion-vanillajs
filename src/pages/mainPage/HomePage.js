4d9f07c8cea8e0c59b3c1469d6b5e600ab54d381
JIY00N2 <angella990825@gmail.com>
2024-01-20 14:50:48 +0900

    style: 문구 띄어쓰기 반영

--- src/pages/mainPage/HomePage.js
+++ src/pages/mainPage/HomePage.js
@@ -2,10 +2,10 @@ import { validateComponent } from '../../utils/validation';
 
 const HOME_MESSAGE = {
   TITLE: '노션 클로닝 프로젝트',
-  ADD_PAGE: '페이지 추가 버튼으로 새 글을 작성해 보세요.',
-  ADD_BUTTON: '➕로 글을 추가하거나',
-  DELETE_BUTTON: '➖로 글을 삭제할 수 있어요.',
-  BACK_TO_HOME: '유저명을 누르면 홈으로 돌아올 수 있어요!',
+  ADD_PAGE: ' 페이지 추가 버튼으로 새 글을 작성해 보세요.',
+  ADD_BUTTON: ' ➕로 글을 추가하거나',
+  DELETE_BUTTON: ' ➖로 글을 삭제할 수 있어요.',
+  BACK_TO_HOME: ' 유저명을 누르면 홈으로 돌아올 수 있어요!',
 };
 
 export default function HomePage({ $target }) {
